0b01fd167c36617a6bd0c3339e589fdfd650c859
Alexey Yudin <alexey@Alexeys-MacBook-Pro.local>
2022-11-30 21:41:08 +0100

    impovements

--- components/Box2.js
+++ components/Box2.js
@@ -1,15 +1,12 @@
 export default function () {
-    return (
-        <div >
-            <img src="/img_1.png" alt=""/>
-            <div className={"columns is-mobile"}>
-                <h1 >KRAKOW
-                    KLUB RE</h1>
-                <h2>Ціна
-                    Cena</h2>
-                <h2>/  60 zl</h2>
-            </div>
-
-        </div>
-    )
+  return (
+    <>
+      <img src="/img_1.png" alt="" />
+      <div className={"is-mobile"}>
+        <h1>KRAKOW KLUB RE</h1>
+        <h2>Ціна Cena</h2>
+        <h2>/ 60 zl</h2>
+      </div>
+    </>
+  );
 }
