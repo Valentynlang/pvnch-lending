316c5fba15431f80f0dae7456aeda3cf59402201
Alexei Iudin <alexey.y.yudin@gmail.com>
2022-12-01 10:32:08 +0100

    add facebook verification to head section

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,36 @@
-import box from "../components/Box";
 import Box from "../components/Box";
 import Box2 from "../components/Box2";
 import Box3 from "../components/Box3";
 import Box4 from "../components/Box4";
+import Head from 'next/head'
 
 // import styles from 'styles/Box.module.css'
 
 const Index = () => {
   return (
-    <div className={"container p-4"}>
-      <div className="block">
-        <Box />
-      </div>
+    <>
+      <Head>
+        <meta name="facebook-domain-verification" content="bfadd8mluxvoi6c1tpp82w4w38e50y" />
+      </Head>
 
-      <div className="block">
-        <Box2 />
-      </div>
+      <div className={"container p-4"}>
+        <div className="block">
+          <Box />
+        </div>
 
-      <div className="block">
-        <Box3 />
-      </div>
+        <div className="block">
+          <Box2 />
+        </div>
 
-      <div className="block">
-        <Box4 />
+        <div className="block">
+          <Box3 />
+        </div>
+
+        <div className="block">
+          <Box4 />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
