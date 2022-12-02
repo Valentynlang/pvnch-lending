893330952f9e40b1bd39a81bf633259ea8d81752
Alexei Iudin <alexey.y.yudin@gmail.com>
2022-12-02 15:47:43 +0100

    add pixel scripts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import Script from 'next/script'
 import "/styles/global.scss";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script src={"/scripts/pixel.js"}/>
+      <Component {...pageProps} />
+    </>
+  );
 }
