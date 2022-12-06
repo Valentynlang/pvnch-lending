76611645976dc0deb2ea23c761b36b9388dc506f
Alexei Iudin <alexey.y.yudin@gmail.com>
2022-12-06 11:09:24 +0100

    add tik-tok pixel

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script src={"/scripts/pixel.js"}/>
+      <Script src={"/scripts/tik-tok-pixel.js"}/>
       <Component {...pageProps} />
     </>
   );
